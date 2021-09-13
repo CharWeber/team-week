50ed1e93699f194d0ee3ab3aa140510ed4b11e95
Charlie Weber <charlestweber@gmail.com>
2021-09-13 14:26:55 -0700

    add art institute API call functions

---
+++ src/ArtInstitute.js
@@ -0,0 +1,30 @@
+export default class ArtInstitute {
+
+  static searchArt(search) {
+    return fetch(`https://api.artic.edu/api/v1/artworks/search?q=${search}&fields=id,title,image_id&limit=10`)
+      .then(function (response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function (error) {
+        return error;
+      });
+  }
+
+
+  // api call url: https://api.artic.edu/api/v1/artworks/[id]
+  static searchObject(id){
+    return fetch(`https://api.artic.edu/api/v1/artworks/${id}`)
+    .then(function(response){
+      if (!response.ok){
+        throw Error(response.statusText)
+      }
+      return response.json();
+    })
+    .catch(function (error) {
+      return error
+    })
+  }
+}
