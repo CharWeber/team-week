1d0bab560768a9f7ead23871afb7bae45e39bb2e
Charlie Weber <charlestweber@gmail.com>
2021-09-13 14:37:35 -0700

    updates index.js and index.html

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-//import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
-//import anime from 'animejs/lib/anime.es.js'
-=======
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -37,4 +30,3 @@ $(document).ready(function () {
       });
   });
 });
->>>>>>> ArtInstituteService
