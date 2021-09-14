0ad14b3b0857c9dbed30f8071a76d4d099ea3400
toddmac12 <toddmac12@gmail.com>
2021-09-14 13:44:50 -0400

    edits to metMuseum.js and index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 // import anime from 'animejs/lib/anime.es.js'
 import ArtInstitute from './ArtInstitute';
+import metMuseum from './metMuseum';
 
 function displayArt(object) {
   $("#artInList").append(`
@@ -12,12 +13,25 @@ function displayArt(object) {
   <li>Style: ${object.data.style_title}<li>
   <li><img src='https://www.artic.edu/iiif/2/${object.data.image_id}/full/843,/0/default.jpg'></li>`);
 }
+function displayMet(object) {
+  $("#metInList").append(`
+  <li>Title: ${object.data.title}</li>
+  <li>Artist: ${object.data.artist_display}</li>
+  <li>Style: ${object.data.style_title}<li>
+  <li><img src='https://collectionapi.metmuseum.org/public/collection/v1/${object.data.image_id}/full/843,/0/default.jpg'></li>`);
+}
 
 $(document).ready(function () {
-  $("#searchBtn").click(function () {
+  $("#searchForm").submit(function (e) {
+    e.preventDefault();
     let search = $('#search').val();
+
+$("#searchForm2").submit(function (e) {
+    e.preventDefault();
+    let search = $('#search2').val();
     // get ids for paitings with search
-    ArtInstitute.searchArt(search)
+    
+ArtInstitute.searchArt(search)
       .then(function (response) {
         const data = response.data;
         data.forEach(function (piece) {
@@ -29,4 +43,15 @@ $(document).ready(function () {
         });
       });
   });
+  metMuseum.searchArt(search)
+    .then(function (response) {
+      const data = response.data;
+      data.forEach(function (piece) {
+        // gathering the art data with artist/history/date etc..
+        metMuseum.searchObject(piece.id)
+          .then(function (object) {
+            displayArt(object);
+          });
+      });
+    });
 });
--- src/metMuseum.js
+++ src/metMuseum.js
@@ -17,14 +17,14 @@ export default class MetMuseum {
  
   static searchObject(id){
     return fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}?isHighlight=true`)
-    .then(function(response){
-      if (!response.ok){
-        throw Error(response.statusText)
-      }
-      return response.json();
-    })
-    .catch(function (error) {
-      return error
-    })
+      .then(function(response){
+        if (!response.ok){
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function (error) {
+        return error;
+      });
   }
 }
